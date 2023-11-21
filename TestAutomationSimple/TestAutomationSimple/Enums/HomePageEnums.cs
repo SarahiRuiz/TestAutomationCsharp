76d10eebe7601074c5d78f9cbdc78c6d7466eb6f
anaruizsolera <ana.ruiz@solera.com>
2023-11-21 14:44:20 -0600

    Automation is ready

--- TestAutomationSimple/TestAutomationSimple/Enums/HomePageEnums.cs
+++ TestAutomationSimple/TestAutomationSimple/Enums/HomePageEnums.cs
@@ -4,7 +4,7 @@ namespace TestAutomationSimple.Enums
 {
     public class HomePageEnums
     {
-        public By LogoPage = By.Id("firstName");
+        public By LogoPage = By.XPath("//img[@src='/images/Toolsqa.jpg']");
         public By FormOption = By.XPath("//div[@class='card mt-4 top-card'][2]");
     }
 }
