808f1b8ed54bc0515c35c8578c893fa7ee960751
anaruizsolera <ana.ruiz@solera.com>
2023-11-23 12:12:10 -0600

    Adding new test case

---
+++ TestAutomationSimple/TestAutomationSimple/Test/RadioButtonTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestAutomationSimple.PageObject;
+
+namespace TestAutomationSimple.Test
+{
+    public class RadioButtonTest : SetUp
+    {
+        public HomePage HomePage = new HomePage();
+        public RadioButtonPage RadioButtonPage = new RadioButtonPage();
+        [Test]
+        public void RadioButtonExample()
+        {
+            HomePage.VerifyHomePage();
+            HomePage.VerifyTotalOfTables();
+            HomePage.GoToRadioButtonPage();
+            RadioButtonPage.VerifyAndClickYesRadioButton();
+        }
+    }
+}
