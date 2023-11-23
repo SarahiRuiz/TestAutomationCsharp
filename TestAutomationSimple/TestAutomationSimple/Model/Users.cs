9f54053b7acd56e15a3854f279f3807bf201082d
anaruizsolera <ana.ruiz@solera.com>
2023-11-23 15:32:31 -0600

    Adding user enums

---
+++ TestAutomationSimple/TestAutomationSimple/Model/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationSimple.Model
+{
+    public enum Users
+    {
+        Billy,
+        Juan,
+        Miguel
+    }
+}
