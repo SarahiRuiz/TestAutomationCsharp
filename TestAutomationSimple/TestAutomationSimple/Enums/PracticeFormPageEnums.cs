0aaf6e38b9813e44323f1c4fac61fb03520bcafb
anaruizsolera <ana.ruiz@solera.com>
2023-11-30 17:00:00 -0600

    Addin enums of radiobutton, checkbox and dynamic elements

--- TestAutomationSimple/TestAutomationSimple/Enums/PracticeFormPageEnums.cs
+++ TestAutomationSimple/TestAutomationSimple/Enums/PracticeFormPageEnums.cs
@@ -7,11 +7,14 @@ namespace TestAutomationSimple.Enums
         public By FirstNameInput = By.Id("firstName");
         public By LastNameInput = By.Id("lastName");
         public By UserEmailInput = By.Id("userEmail");
-        public By GenderOtherRadioButton = By.Id("gender-radio-3");
+        public By GenderRadioButtonDynamic = By.XPath("//label[text()='?']/../input");
         public By TelephoneNumberInput = By.Id("userNumber");
         public By PopUpCalendar = By.ClassName("react-datepicker");
         public By MonthDropDown = By.ClassName("react-datepicker__month-select");
         public By YearDropDown = By.ClassName("react-datepicker__year-select");
         public By CalendarInput = By.Id("dateOfBirthInput");
+        public By SubjectInput = By.Id("subjectsInput");
+        public By HobbiesCheckBoxDynamic = By.XPath("//label[text()='?']/../input");
+        public By SubjectInputOption = By.ClassName("subjects-auto-complete__menu");
     }
 }
