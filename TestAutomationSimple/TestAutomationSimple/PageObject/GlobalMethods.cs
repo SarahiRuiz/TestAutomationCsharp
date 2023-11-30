6ae76e5c22ecbb0f73b174b6a572202847ec986a
anaruizsolera <ana.ruiz@solera.com>
2023-11-30 16:58:09 -0600

    Adding method to click check box and radio button

--- TestAutomationSimple/TestAutomationSimple/PageObject/GlobalMethods.cs
+++ TestAutomationSimple/TestAutomationSimple/PageObject/GlobalMethods.cs
@@ -1,6 +1,8 @@
 ﻿
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
 
 namespace TestAutomationSimple.PageObject
 {
@@ -40,6 +42,59 @@ namespace TestAutomationSimple.PageObject
                 return wait.Until(d =>
                 {
                     element.Click();
+                    return true;
+                });
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool ScrollToElement(IWebElement element, int timeOut = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOut));
+            return wait.Until(d => WaitToScrollToElement(wait, element));
+        }
+        public bool WaitToScrollToElement(WebDriverWait wait, IWebElement element)
+        {
+            try
+            {
+                return wait.Until(d =>
+                {
+                    var scrollOrigin = new WheelInputDevice.ScrollOrigin
+                    {
+                        Element = element,
+                        XOffset = 0,
+                        YOffset = -50
+                    };
+                    new Actions(driver)
+                        .ScrollFromOrigin(scrollOrigin, 0, 200)
+                        .Perform();
+                    Thread.Sleep(1000);
+                    return true;
+                });
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }            
+        }
+        public IWebElement DynamicToIWebElement(By element, String value)
+        {
+            return driver.FindElement(By.XPath(element.Criteria.Replace("?", value)));
+        }
+        public bool ClickRadioButtonOrCheckBox(IWebElement element, int timeOut = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOut));
+            return wait.Until(d => WaitToClickRadioButtonOrCheckBox(wait, element));
+        }
+        public bool WaitToClickRadioButtonOrCheckBox(WebDriverWait wait, IWebElement element)
+        {
+            try
+            {
+                return wait.Until(d =>
+                {                    
+                    action.Click(element).Build().Perform();
                     return true;
                 });
             }
