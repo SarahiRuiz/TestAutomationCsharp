3ee80341d55411fa639d0ea3bed43a1de503af7e
anaruizsolera <ana.ruiz@solera.com>
2023-12-05 15:07:25 -0600

    Enum to validate

--- TestAutomationSimple/TestAutomationSimple/Enums/RadioButtonPageEnums.cs
+++ TestAutomationSimple/TestAutomationSimple/Enums/RadioButtonPageEnums.cs
@@ -5,5 +5,6 @@ namespace TestAutomationSimple.Enums
     public class RadioButtonPageEnums
     {
         public By RadioButtonOption = By.XPath("//label[text()='?']/../input");
+        public By RadioButtonTitle = By.XPath("//p[text()='You have selected ']//span[text()='?']");
     }
 }
