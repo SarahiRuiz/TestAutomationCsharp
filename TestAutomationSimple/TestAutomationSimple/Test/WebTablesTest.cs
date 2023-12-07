48d0340a4e8ad18cd7860fe105d8fe290743b29f
anaruizsolera <ana.ruiz@solera.com>
2023-12-07 16:27:10 -0600

    Adding test for web tables

---
+++ TestAutomationSimple/TestAutomationSimple/Test/WebTablesTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TestAutomationSimple.Data;
+using TestAutomationSimple.Model;
+using TestAutomationSimple.PageObject;
+
+namespace TestAutomationSimple.Test
+{    
+    public class WebTablesTest : SetUp
+    {
+        public HomePage HomePage = new HomePage();
+        public WebTablesPage WebTablesPage = new WebTablesPage();
+        [Test, Category("Regression Testing")]
+        [TestCaseSource(typeof(WebTableDataSource), nameof(WebTableDataSource.WebTableTest))]
+        public void WebTableTest_(WebTableOptions webTableOptions, WebTableData validData)
+        {
+            HomePage.VerifyHomePage();
+            HomePage.VerifyTotalOfTables();
+            HomePage.GoToWebTablesPage();
+            WebTablesPage.Actions(webTableOptions, validData);
+        }   
+    }
+}
