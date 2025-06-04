df9fcfd93bfda7cf063486e655370d9e34662323
Ana Ruiz <Ana.Ruiz@Solera.com>
2025-06-04 10:16:55 -0600

    Fixing set up driver manager

--- TestAutomationSimple/PageObject/SetUp.cs
+++ TestAutomationSimple/PageObject/SetUp.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
 namespace TestAutomationSimple.PageObject
@@ -14,13 +15,9 @@ namespace TestAutomationSimple.PageObject
         [SetUp]
         public void SetUpInitial()
         {
-            var options = new ChromeOptions();
-            var driverPath = @"C:\Users\Ana.Ruiz\source\MyTraning\TestAutomation2\TestAutomationSimple\TestAutomationSimple\bin\Debug\net6.0\Chrome\134.0.6998.90\X64"; 
-            driver = new ChromeDriver(driverPath, options);
-
-
-            /*new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig(), "134.0.6998.90");
-            driver = new ChromeDriver();*/
+            // Configura el driver con WebDriverManager
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            IWebDriver driver = new ChromeDriver();
             driver.Url = "https://demoqa.com/";
             action = new Actions(driver);
             driver.Manage().Window.Maximize();
